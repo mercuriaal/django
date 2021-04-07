48b5c7e0fd4c357c2c388b9975698ce43aec2f4b
Ilya Lazarev <ilya.v.lazarev@gmail.com>
2021-04-07 20:26:13 +0300

    first commit

--- request-handling/landing/app/views.py
+++ request-handling/landing/app/views.py
@@ -2,31 +2,38 @@ from collections import Counter
 
 from django.shortcuts import render
 
-# Для отладки механизма ab-тестирования используйте эти счетчики
-# в качестве хранилища количества показов и количества переходов.
-# но помните, что в реальных проектах так не стоит делать
-# так как при перезапуске приложения они обнулятся
+
 counter_show = Counter()
 counter_click = Counter()
 
 
 def index(request):
-    # Реализуйте логику подсчета количества переходов с лендига по GET параметру from-landing
+    version = request.GET.get('from-landing')
+    if version == 'original':
+        counter_click['original'] += 1
+    elif version == 'test':
+        counter_click['test'] += 1
     return render(request, 'index.html')
 
 
 def landing(request):
-    # Реализуйте дополнительное отображение по шаблону app/landing_alternate.html
-    # в зависимости от GET параметра ab-test-arg
-    # который может принимать значения original и test
-    # Так же реализуйте логику подсчета количества показов
-    return render(request, 'landing.html')
+    version = request.GET.get('ab-test-arg')
+    if version == 'original':
+        counter_show['original'] += 1
+        return render(request, 'landing.html')
+    elif version == 'test':
+        counter_show['test'] += 1
+        return render(request, 'landing_alternate.html')
 
 
 def stats(request):
-    # Реализуйте логику подсчета отношения количества переходов к количеству показов страницы
-    # Для вывода результат передайте в следующем формате:
+    test_conversion = 0
+    original_conversion = 0
+    if counter_show['test'] != 0:
+        test_conversion = counter_click['test']/counter_show['test']
+    if counter_show['original'] != 0:
+        original_conversion = counter_click['original'] / counter_show['original']
     return render(request, 'stats.html', context={
-        'test_conversion': 0.5,
-        'original_conversion': 0.4,
+        'test_conversion': test_conversion,
+        'original_conversion': original_conversion,
     })
